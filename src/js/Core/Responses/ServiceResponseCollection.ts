ee8c6ba629f053c630c38e3feea9380435268148
Gautam Singh <gautamsi@users.noreply.github.com>
2018-09-30 13:37:32 +0530

    closes #256

--- src/js/Core/Responses/ServiceResponseCollection.ts
+++ src/js/Core/Responses/ServiceResponseCollection.ts
@@ -1,14 +1,49 @@
-﻿import {ServiceResponse} from "./ServiceResponse";
-import {Strings} from "../../Strings";
-import {ArgumentOutOfRangeException} from "../../Exceptions/ArgumentException";
-import {ServiceResult} from "../../Enumerations/ServiceResult";
-import {EwsLogging} from "../EwsLogging";
-export class ServiceResponseCollection<TResponse extends ServiceResponse> { // IEnumerable<TResponse> where TResponse : ServiceResponse
-    get Count(): number { return this.responses.length; }
-    get Responses(): TResponse[] { return this.responses; }
-    get OverallResult(): ServiceResult { return this.overallResult; }
-    private responses: TResponse[] = [];// System.Collections.Generic.List<T>;
+﻿import { ArgumentOutOfRangeException } from "../../Exceptions/ArgumentException";
+import { EwsLogging } from "../EwsLogging";
+import { IEnumerable } from "../../Interfaces/IEnumerable";
+import { ServiceResponse } from "./ServiceResponse";
+import { ServiceResult } from "../../Enumerations/ServiceResult";
+import { Strings } from "../../Strings";
+
+/**
+ * Represents a strogly typed list of service responses.
+ * @sealed
+ * @typeparam   {TResponse}     The type of response stored in the list.
+ */
+export class ServiceResponseCollection<TResponse extends ServiceResponse> implements IEnumerable<TResponse> {
+
+    private responses: TResponse[] = [];
+
     private overallResult: ServiceResult = ServiceResult.Success;
+
+    /**
+     * Gets the total number of responses in the list.
+     */
+    get Count(): number {
+        return this.responses.length;
+    }
+
+    get Responses(): TResponse[] { return this.responses; }
+
+    /**
+     * Gets a value indicating the overall result of the request that generated this response collection.
+     * If all of the responses have their Result property set to Success, OverallResult returns Success.
+     * If at least one response has its Result property set to Warning and all other responses have their Result property set to Success, OverallResult returns Warning. 
+     * If at least one response has a its Result set to Error, OverallResult returns Error.
+     */
+    get OverallResult(): ServiceResult { return this.overallResult; }
+
+    /**
+     * @internal Initializes a new instance of the **ServiceResponseCollection<TResponse>** class.
+     */
+    constructor() {
+    }
+
+    /**
+     * @internal Adds specified response.
+     *
+     * @param   {TResponse}   response   The response.
+     */    
     Add(response: TResponse): void {
         EwsLogging.Assert(
             response != null,
@@ -21,8 +56,23 @@ export class ServiceResponseCollection<TResponse extends ServiceResponse> { // I
 
         this.responses.push(response);
     }
-    GetEnumerator(): any { throw new Error("ServiceResponseCollection.ts - GetEnumerator : Not implemented."); }
-    __thisIndexer(index: number) {
+
+    /**
+     * Gets an enumerator that iterates through the elements of the collection.
+     *
+     * @return  {TResponse[]}      An IEnumerator for the collection.
+     */    
+    GetEnumerator(): TResponse[] {
+        return this.responses;
+    }
+
+    /**
+     * Gets the response at the specified index.
+     *
+     * @param   {number}        index   The zero-based index of the response to get.
+     * @return  {TResponse}     The response at the specified index.
+     */
+    __thisIndexer(index: number): TResponse {
         if (index < 0 || index >= this.Count) {
             throw new ArgumentOutOfRangeException("index", Strings.IndexIsOutOfRange);
         }
