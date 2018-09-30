a7548503b0fa6a7b78d755bae096f9044bb71eaf
Gautam Singh <gautamsi@users.noreply.github.com>
2018-09-30 16:33:55 +0530

    additional approach for uuid v4 only operation while relaxing parsing of generic UUID used in MAPI Property. closes #267 and closes #268

--- src/js/Guid.ts
+++ src/js/Guid.ts
@@ -1,7 +1,12 @@
-import { StringHelper } from "./ExtensionMethods";
 import * as uuid from 'uuid';
+import { EwsLogging } from "./Core/EwsLogging";
+import { StringHelper } from "./ExtensionMethods";
+
 /** Guid proxy class */
 export class Guid {
+
+	static ParseStrict: boolean = false;
+
 	static Empty: Guid = new Guid();
 	private guid: string = '00000000-0000-0000-0000-000000000000';
 	//private regx = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
@@ -10,24 +15,31 @@ export class Guid {
 	constructor();
 	constructor(str: string);
 	constructor(str?: string) {
-		let regx = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+		let regxStrict = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+		let regxRelax = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 		if (arguments.length > 0) {
 			if (StringHelper.IsNullOrEmpty(str) || str === null) {
 				throw new TypeError("Guid.ctor - invalid input");
 			}
 			//str = str.replace("-", "").replace("{", "").replace("}", "").toLowerCase();
 			str = str.replace("{", "").replace("}", "").toLowerCase();
-			if (regx.test(str)) {
+			if (regxStrict.test(str)) {
 				this.guid = str;
 			} else {
-				throw new TypeError("Guid.ctor - invalid input");
+				if (!Guid.ParseStrict && regxRelax.test(str)) {
+					EwsLogging.DebugLog("info: Guid.ctor - guid is in generic format. if you want to error on non uuid v4 format, set `Guid.ParseStrict = true`")
+					this.guid = str;
+				}
+				else {
+					throw new TypeError("Guid.ctor - invalid input, input not of type uuid v4");
+				}
 			}
 		}
 	}
 	ToString() {
 		return this.guid;
 	}
-	toString(){
+	toString() {
 		return this.guid;
 	}
 
