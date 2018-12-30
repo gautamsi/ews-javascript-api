278f4928af8681911b922f9c49f17bbebfcdb87b
Zach Mathew <zmathew.to@gmail.com>
2018-12-29 21:17:17 -0500

    Handle null when parsing enums so GetUserAvailability can handle null WorkingPeriod DayOfWeek.
    A similar fix was applied to the C# ews-managed-api, see: https://github.com/OfficeDev/ews-managed-api/issues/51

--- test/mocha/Operations/CalendarOperationsTests.ts
+++ test/mocha/Operations/CalendarOperationsTests.ts
@@ -1,4 +1,5 @@
 import {AutodiscoverService, WebCredentials, ExchangeService, ConflictResolutionMode, Guid, ExtendedPropertyDefinition, MapiPropertyType, PropertySet, ExchangeVersion, ItemView, UserSettingName, EwsLogging, Uri, DateTime, WellKnownFolderName, DefaultExtendedPropertySet, BasePropertySet}  from "../../../src/js/ExchangeWebService";
+import {TimeWindow, AttendeeInfo, AvailabilityData, ServiceResponseCollection, GetUserAvailabilityResults}  from "../../../src/js/ExchangeWebService";
 import {MockXHRApi} from "../../MockXHRApi";
 import {MockXHRData} from "../../MockXHRData";
 
@@ -7,7 +8,7 @@ import chai = require('chai');
 chai.use(chaiAsPromised);
 var expect = chai.expect;
 chai.should();
-describe.skip("Calendar Operation tests", () => {
+describe("Calendar Operation tests", () => {
     var MyPropertySetId: Guid = new Guid("{C11FF724-AA03-4555-9952-8FA248A11C3E}");
     var exch = new ExchangeService(ExchangeVersion.Exchange2013);
     exch.Url = new Uri("https://fake");
@@ -37,7 +38,7 @@ describe.skip("Calendar Operation tests", () => {
             });
     });
 
-    it("Create extended property ", (done) => {
+    it.skip("Create extended property ", (done) => {
         mockXhr.requestXml = [MockXHRData.Operations.ItemOperations.FindItemRequest1ItemView];
         mockXhr.responseXml = [MockXHRData.Operations.ItemOperations.FindItemRequest1ItemViewResponse];
         var PR_TRANSPORT_MESSAGE_HEADERS = new ExtendedPropertyDefinition(MapiPropertyType.String, 0x007D);
@@ -63,6 +64,28 @@ describe.skip("Calendar Operation tests", () => {
                         });
                 }
             });
+    });
+
+    it("GetUserAvailability handles empty WorkingPeriod DayOfWeek", (done) => {
+        mockXhr.responseXml = [MockXHRData.Operations.CalendarOperations.GetUserAvailabilityRequestResponseWithEmptyDayOfWeek];
+
+        exch.XHRApi = mockXhr;
+
+        var timeWindow = new TimeWindow(
+            new DateTime(new Date()),
+            new DateTime(new Date(new Date().getTime() + (60 * 60 * 24 * 1000)))
+        );
+        var attendees = [new AttendeeInfo('test@example.com')];
+        exch.GetUserAvailability(attendees, timeWindow, AvailabilityData.FreeBusy).then(
+            function (response: GetUserAvailabilityResults) {
+                chai.assert.equal(response.AttendeesAvailability.Count, 1);
+                chai.assert.deepEqual(
+                    response.AttendeesAvailability.Responses[0].WorkingHours.DaysOfTheWeek,
+                    []
+                );
+                done();
+            }
+        );
     });
 
 });
